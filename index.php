537a52b07d32d8a715d25b5f3776ef8782ed311c
Jonas Lilja <jonas@lilja.io>
2017-07-20 12:57:48 +0200

    autoloading with psr-4 added

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Jcbl\Booliwrapper\Booli as Booli;
+
 /*
 	Example made by Jonas Lilja (lilja.io)
 	You can find the repository for this project at https://github.com/jlilja/Booli-php-wrapper
 */
-
-require 'Booli.php';
 
 $booli = new Booli();
 $booli->setApiKey('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
@@ -15,6 +17,6 @@ $filters = [
 	'maxListPrice' => 4000000,
 ];
 
-$test = $booli->getListing('Stockholm')->withImages();
+$test = $booli->getListing('Stockholm');
 
 echo json_encode($test->response);
