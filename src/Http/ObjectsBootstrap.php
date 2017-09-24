9dfeb6184afcb29502912fbce0748216c9de109e
Jonas Lilja <jonas@lilja.io>
2017-09-24 11:14:45 +0200

    moved methods to super class

--- src/Http/ObjectsBootstrap.php
+++ src/Http/ObjectsBootstrap.php
@@ -2,12 +2,35 @@
 
 namespace Jcbl\Booliwrapper\Http;
 
-use Jcbl\Booliwrapper\Booli;
+use Jcbl\Booliwrapper\Interfaces\ListingsInterface;
+use Jcbl\Booliwrapper\Authentication;
 
-class ObjectsBootstrap
+class ObjectsBootstrap implements ListingsInterface
 {
-	public function __construct(Booli $class)
+	public function __construct(Authentication $authentication)
 	{
-		$this->auth = $class->auth;
+		$this->auth = $authentication;
+	}
+
+	public function all($filters)
+	{
+		$params = http_build_query($this->auth->getAuthInfo());
+
+		if ($filters['filters']) {
+			$filters = http_build_query($filters);
+		}
+
+		$url = $this->host . "?" . $params . '&' . $filters;
+
+		return $this->auth->request($url);
+	}
+
+	public function single($id)
+	{
+		$params = $this->auth->getAuthInfo();
+
+		$url = $this->host . $id . "?&" . http_build_query($params);
+
+		return $this->auth->request($url);
 	}
 }
--- src/Http/Sold.php
+++ src/Http/Sold.php
@@ -2,33 +2,7 @@
 
 namespace Jcbl\Booliwrapper\Http;
 
-use Jcbl\Booliwrapper\Interfaces\Listings;
-
-class Sold extends ObjectsBootstrap implements Listings
+class Sold extends ObjectsBootstrap
 {
 	protected $host = 'http://api.booli.se/sold/';
-
-	public function all($filters)
-	{
-		$params = $this->auth->getAuthInfo();
-
-		if ($filters['filters']) {
-			foreach ($filters as $filter => $value) {
-				$params[$filter] = $value;
-			}
-		}
-
-		$url = $this->host . "?" . http_build_query($params);
-
-		return $this->auth->request($url);
-	}
-
-	public function single($id)
-	{
-		$params = $this->auth->getAuthInfo();
-
-		$url = $this->host . $id . "?&" . http_build_query($params);
-
-		return $this->auth->request($url);
-	}
 }
