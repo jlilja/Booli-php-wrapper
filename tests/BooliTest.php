f9136474cad793a85a5d04ea4e6ecb359305aaba
Jonas Lilja <jonas@lilja.io>
2017-09-30 16:11:05 +0200

    removed tests for now

--- tests/BooliTest.php
+++ tests/BooliTest.php
@@ -14,25 +14,5 @@ class BooliTest extends PHPUnit_Framework_TestCase {
 	{
 		$this->booli = new Booli(getenv('CALLER_ID'), getenv('API_KEY'));
 	}
- 
-	public function testBooliHasMethodListing()
-	{
-		$this->assertTrue(method_exists($this->booli, 'listing'), 'Class does not have method listing');
-	}
-
-	public function testBooliHasMethodSingle()
-	{
-		$this->assertTrue(method_exists($this->booli, 'single'), 'Class does not have method single');
-	}
-
-	public function testBooliHasMethodWithImages()
-	{
-		$this->assertTrue(method_exists($this->booli, 'withImages'), 'Class does not have method withImages');
-	}
-
-	public function testBooliHasMethodGetImageSize()
-	{
-		$this->assertTrue(method_exists($this->booli, 'getImageSize'), 'Class does not have method getImageSize');
-	}
 
 }
